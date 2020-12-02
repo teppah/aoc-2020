f4bd36e8bf757c8b4c8bff9ed862b446353dbe3f
Yi Feng Yang <yifeng@yfyang.dev>
2020-12-01 20:23:55 -0500

    Do part 2

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,17 @@ fn main() {
 fn report_repair_1() {
     let lines = fs::read_to_string("inputs/1.txt")
         .expect("did not find file!");
-    let numbers: Vec<i32> = lines
+    let list: Vec<i32> = lines
         .lines()
         .into_iter()
         .map(|num| {
             return num.parse().unwrap();
         })
         .collect();
-    println!("list:\n{:?}", numbers);
+    println!("list:\n{:?}", list);
 
     let mut numbers =
-        HashSet::<i32>::from_iter(numbers.iter().cloned());
+        HashSet::<i32>::from_iter(list.iter().cloned());
 
     let total = 2020;
     println!("---TWO NUMBERS---");
@@ -29,6 +29,23 @@ fn report_repair_1() {
             println!("numbers: {} and {}", val, remainder);
             println!("multiply both: {}", val * remainder);
             break;
+        }
+    }
+    println!("---THREE NUMBERS---");
+
+    'outer: for first_val in list.iter() {
+        let remainder = total - first_val;
+        for second_val in list.iter() {
+            if second_val == first_val {
+                continue;
+            }
+            let new_remainder = remainder - second_val;
+            if numbers.contains(&new_remainder) {
+                println!("vals are {}, {} and {}", first_val, second_val, new_remainder);
+                println!("add to {}", first_val + second_val + new_remainder);
+                println!("multiply: {}", first_val * second_val * new_remainder);
+                break 'outer;
+            }
         }
     }
 }
