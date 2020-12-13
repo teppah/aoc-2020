0560d2d11c0f2705372820e3ae7c1172183c88eb
Yi Feng Yang <yifeng@yfyang.dev>
2020-12-12 20:24:34 -0500

    Day10 part2

--- src/day_10.rs
+++ src/day_10.rs
@@ -1,8 +1,9 @@
 use std::collections::HashSet;
+use itertools::Itertools;
 
 pub fn adapter_array() {
     let input = std::fs::read_to_string("inputs/10.txt").unwrap();
-    let joltages: HashSet<i32> = input.lines()
+    let mut joltages: HashSet<i64> = input.lines()
         .map(|line| line.parse().unwrap())
         .collect();
     let max = joltages.iter().cloned().max().unwrap();
@@ -32,4 +33,20 @@ pub fn adapter_array() {
         }
     }
     println!("low: {}, high: {}, product: {}", one_jolt, three_jolt, one_jolt * three_jolt);
+    joltages.insert(0);
+    let mut joltages =
+        joltages.iter().cloned().collect::<Vec<i64>>();
+    joltages.sort_unstable();
+    let final_result = joltages
+        .windows(2)
+        .collect::<Vec<_>>()
+        .split(|n| n[1] - n[0] == 3)
+        .map(|n| match n.len() {
+            4 => 7,
+            3 => 4,
+            2 => 2,
+            _ => 1,
+        })
+        .product::<usize>();
+    println!("Final result: {}", final_result);
 }
